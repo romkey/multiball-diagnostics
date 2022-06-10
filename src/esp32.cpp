a3ace1efa582c22d398b219be0629ef88ae49bc2
John Romkey <58883+romkey@users.noreply.github.com>
2022-06-10 10:07:39 -0700

    rewrote ESP32 code to avoid fuses, show cores, psram, Arduino core version
    updated I2C devices

--- src/esp32.cpp
+++ src/esp32.cpp
@@ -3,41 +3,25 @@
 #include <Arduino.h>
 #include <Esp.h>
 
-
-extern "C" {
-#include "soc/efuse_reg.h"
-};
-
-#if 0
-static int cpu_vevision() {
-  return REG_READ(EFUSE_BLK0_RDATA3_REG) >> (EFUSE_RD_CHIP_VER_RESERVE_S) && EFUSE_RD_CHIP_VER_RESERVE_V;
-}
-#endif
-
-int cpu_version() {
-  //  return (REG_READ(EFUSE_BLK0_RDATA3_REG) >> (EFUSE_RD_CHIP_VER_RESERVE_S)&&EFUSE_RD_CHIP_VER_RESERVE_V) ;
-
-  return REG_READ(EFUSE_BLK0_RDATA3_REG) &&  EFUSE_RD_CHIP_VER_REV1;
-}
-
-
 void esp_output() {
   Serial.println();
 
-  Serial.printf("SDK version %s\n", ESP.getSdkVersion());
-  Serial.printf("Flash Chip Speed %d\n", ESP.getFlashChipSpeed());
-  Serial.printf("CPU version %d\n", cpu_version());
-  Serial.print("REG_READ(EFUSE_BLK0_RDATA3_REG) ");
-  Serial.println(REG_READ(EFUSE_BLK0_RDATA3_REG), BIN);
-  Serial.print("EFUSE_RD_PART_VER_RESERVE_S ");
-  Serial.println( EFUSE_RD_BLK3_PART_RESERVE_S, BIN);
-  Serial.print("EFUSE_RD_PART_VER_RESERVE_V ");
-  Serial.println( EFUSE_RD_BLK3_PART_RESERVE_V, BIN);
-  Serial.println();
-  Serial.print("Chip Revision (official version): ");
-  Serial.println(cpu_version());
-  Serial.print("Chip Revision from shift Opration ");
-  Serial.println(REG_READ(EFUSE_BLK0_RDATA3_REG) >> 15, BIN);
+  Serial.print("ESP-IDF version "); Serial.println(esp_get_idf_version());
+  Serial.print("Arduino Core version "); Serial.print(ESP_ARDUINO_VERSION_MAJOR); Serial.print("."); Serial.print(ESP_ARDUINO_VERSION_MINOR); Serial.print("."); Serial.println(ESP_ARDUINO_VERSION_PATCH);
+  Serial.print("Flash Chip Speed "); Serial.println(ESP.getFlashChipSpeed());
+  Serial.print("Model "); Serial.print(ESP.getChipModel()); Serial.print(" revision "); Serial.println(ESP.getChipRevision());
+  Serial.print("Cores "); Serial.println(ESP.getChipCores());
+  Serial.print("PSRAM "); Serial.println(ESP.getPsramSize());
+
+  uint64_t mac_addr = ESP.getEfuseMac();
+  Serial.print("WIFI MAC address ");
+  Serial.print(mac_addr & 0xFF, HEX); Serial.print(":");
+  Serial.print((mac_addr >> 8) & 0xFF, HEX); Serial.print(":");
+  Serial.print((mac_addr >> 16) & 0xFF, HEX); Serial.print(":");
+  Serial.print((mac_addr >> 24) & 0xFF, HEX); Serial.print(":");
+  Serial.print((mac_addr >> 32) & 0xFF, HEX); Serial.print(":");
+  Serial.println((mac_addr >> 40) & 0xFF, HEX);
+
   Serial.println();
   Serial.println();
 }
